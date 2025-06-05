58b0541d76d1d1c2e615808ee304bba4d1b2348c
rail888 <belokana@hanmail.net>
2025-06-05 15:13:40 +0900

    0002

--- src/main/java/pack/Sample05Application.java
+++ src/main/java/pack/Sample05Application.java
@@ -2,9 +2,16 @@ package pack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Sample05Application {
+	@GetMapping("/")
+	public String hello(){
+		return "hello 시저";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sample05Application.class, args);
